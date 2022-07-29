01c4297231a29d1ec93d97f99922e801f08983ae
Vinícius Vaz <viniciusvaz97@gmail.com>
2022-07-29 02:09:44 -0300

    CardService created

---
+++ src/main/java/cardSystem/API/service/CardService.java
@@ -0,0 +1,12 @@
+package cardSystem.API.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import cardSystem.API.model.Card;
+
+@Service
+public class CardService {
+  private List<Card> Cards;
+}
