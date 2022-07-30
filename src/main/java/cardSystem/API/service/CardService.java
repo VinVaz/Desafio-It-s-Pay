e60b96cdb7e7f0d98df9fca9b9aa2dceaa8c214b
Vinícius Vaz <viniciusvaz97@gmail.com>
2022-07-29 22:50:38 -0300

    fillCardValue method

--- src/main/java/cardSystem/API/service/CardService.java
+++ src/main/java/cardSystem/API/service/CardService.java
@@ -1,12 +1,44 @@
 package cardSystem.API.service;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import cardSystem.API.model.Card;
 
 @Service
 public class CardService {
-  private List<Card> Cards;
+  private List<Card> cards;
+
+  public void createCardList() {
+    if (cards == null) {
+      cards = new ArrayList<>();
+    }
+  }
+
+  public boolean isJSONValid(String jsonString) {
+    try {
+      return new ObjectMapper().readTree(jsonString) != null;
+    } catch (IOException e) {
+      return false;
+    }
+  }
+
+  private long parseId(JSONObject card) {
+    return Long.valueOf((int) card.get("id"));
+  }
+
+  private void fillCardValues(JSONObject jsonCard, Card card) {
+    String numero = (String) jsonCard.get("numero");
+    String nomeUsuario = (String) jsonCard.get("nomeUsuario");
+    String codigoSeguranca = (String) jsonCard.get("codigoSeguranca");
+
+    String dataValidade = (String) jsonCard.get("dataValidade");
+
+  }
 }
