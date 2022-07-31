95c958a41cc01d2c8d72dcdd8b56346153b6f04b
Vinícius Vaz <viniciusvaz97@gmail.com>
2022-07-30 22:58:07 -0300

    update

--- src/main/java/cardSystem/API/service/CardService.java
+++ src/main/java/cardSystem/API/service/CardService.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+
 import org.json.simple.JSONObject;
 import org.springframework.stereotype.Service;
 
@@ -33,13 +36,32 @@ public class CardService {
     return Long.valueOf((int) card.get("id"));
   }
 
+  private YearMonth parseDataValidade(String dataValidade) {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern(Card.DATA_VALIDADE_FORMATO);
+    return YearMonth.parse(dataValidade, formatter);
+  }
+
   public Card create(JSONObject jsonCard) {
     String numero = (String) jsonCard.get("numero");
     String nomeUsuario = (String) jsonCard.get("nomeUsuario");
     String codigoSeguranca = (String) jsonCard.get("codigoSeguranca");
     String dataValidade = (String) jsonCard.get("dataValidade");
 
-    Card card = new Card();
+    Card card = new Card(numero, nomeUsuario, codigoSeguranca, parseDataValidade(dataValidade));
     return card;
   }
+
+  public Card update(Card card, JSONObject jsonCard) {
+    String numero = (String) jsonCard.get("id");
+    String nomeUsuario = (String) jsonCard.get("nomeUsuario");
+    String codigoSeguranca = (String) jsonCard.get("codigoSeguranca");
+    String dataValidade = (String) jsonCard.get("dataValidade");
+
+    card.setNumero(numero != null ? numero : card.getNumero());
+    card.setNomeUsuario(nomeUsuario != null ? nomeUsuario : card.getNomeUsuario());
+    card.setCodigoSeguranca(codigoSeguranca != null ? codigoSeguranca : card.getCodigoSeguranca());
+    card.setDataValidade(dataValidade != null ? parseDataValidade(dataValidade) : card.getDataValidade());
+    return card;
+  }
+
 }
